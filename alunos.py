92517ae53177ca147d9ccc4c9de29c9b749bacd7
Guilherme Santos <guipinela.santos@gmail.com>
2021-03-21 15:07:44 +0000

    commit

--- alunos.py
+++ alunos.py
@@ -1,3 +1,4 @@
+from time import sleep
 print('-' * 60)
 print('Media dos alunos'.center(60))
 print('-' * 60)
@@ -7,5 +8,18 @@ while notas != 0:
     notas = float(input('Notas dos alunos e digite 0 para parar de digitar notas: '))
     numalunos += 1
     totnotas += notas
+    if maiornota < notas and menornota < notas:
+        maiornota = notas
+        menornota = notas
+    elif notas > maiornota:
+        maiornota = notas
+    elif notas < menornota:
+        if notas != 0:
+            menornota = notas
 media = totnotas / numalunos
-print(f'A média de as notas dos alunos é {media:.2f}')
+print('Calculando...')
+sleep(1)
+print('-' * 60)
+print(f'A média das notas dos alunos é {media:.2f}')
+print(f'A maior nota dos alunos foi {maiornota}')
+print(f'A menor nota dos alunos foi {menornota}')
